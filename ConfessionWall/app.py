99c837855cc628d393a4d260b05194517a9da4d9
Python_Service <smujm@qq.com>
2020-08-25 17:36:18 +0800

    modify Confession

--- ConfessionWall/app.py
+++ ConfessionWall/app.py
@@ -8,25 +8,41 @@
 """
 from tornado import web, ioloop, httpserver
 
+# 留言
+
+MESSAGES = [
+	{'id': 1, 'name': 'xm', 'time': '2020-8-24 21:16:00', 'content': '爱你', 'num': 1}
+]
+
 
 # 视图
 class MainPageHandler(web.RequestHandler):
 	def get(self, *args, **kwargs):
 		self.write('hello xy')
 
+
 # 表白
 class ConfessionHandler(web.RequestHandler):
 	def get(self, *args, **kwargs):
-		self.render('')
+		self.render('index.html')
+	
 	def post(self, *args, **kwargs):
+		pass
+
+
+# 设置
+settings = {
+	'template_path': '前端文件\\templates',  # 模板文件路径
+	'static_path': '前端文件\\statics',  # 静态文件路径
+}
 
 # 模板
 
 # 路由系统
 app = web.Application(
 	[
-		(r'/', MainPageHandler),
-	]
+		(r'/', ConfessionHandler),
+	], **settings
 )
 
 if __name__ == '__main__':
